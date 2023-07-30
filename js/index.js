af35708c5706585e630b8178ef1816775fe48dfc
Yamil <yoms.tf@gmail.com>
2023-07-30 15:53:13 +0100

    added toggle between grid view and list view

--- js/index.js
+++ js/index.js
@@ -48,7 +48,7 @@ function showCards() {
     const cards = document.querySelectorAll('.card');
     for (let i = 0; i < numberOfCards; i++) {
         if (i >= (pageNumber - 1) * cardsPerPage && i < pageNumber * cardsPerPage) {
-            cards[i].style.display = 'block';
+            cards[i].style.display = 'flex';
         } else {
             cards[i].style.display = 'none';
         }
@@ -57,4 +57,32 @@ function showCards() {
 
 // Show first page
 showCards();
+
+
+// Logic that change the display to a grid or a list of cards
+let cardContainer = document.querySelector(".grid-container")
+
+let displayInGrid = document.querySelector(".display-in-grid")
+displayInGrid.onclick = function(event) {
+    event.preventDefault();
+    cardContainer.style.gridTemplateColumns = "repeat(3, 1fr)"
+    cardContainer.style.placeContent = null;  
+    let images = document.querySelectorAll(".card-img")
+    for (let i = 0; i < images.length; i++) {
+        images[i].style.width = null; 
+        images[i].style.height = null;  
+    }
+}
+
+let displayInList = document.querySelector(".display-in-list")
+displayInList.onclick = function(event) {
+    event.preventDefault();
+    cardContainer.style.gridTemplateColumns = "repeat(1, 0.5fr)"
+    cardContainer.style.placeContent = "center"
+    let images = document.querySelectorAll(".card-img")
+    for (let i = 0; i < images.length; i++) {
+        images[i].style.width = "700px"
+        images[i].style.height = "300px"
+    }
+}
 
